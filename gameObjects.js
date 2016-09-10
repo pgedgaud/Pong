2844c23b20a7566216d3bd0fe089c3d51dc33fdb
Logan Komanetz <logan.komanetz@gmail.com>
2016-09-09 20:59:51 -0500

    Fixes bug with ball being behind paddle.

--- gameObjects.js
+++ gameObjects.js
@@ -27,7 +27,7 @@ Ball.prototype.updatePosition = function() {
 Ball.prototype.checkCollisionsWith = function(paddles, canvas) {
     var lastPaddleHit = -1;
     var isBallInGoal = false;
-    if (this.x < paddles[0].x + paddles[0].thickness) {
+    if (this.x == paddles[0].x + paddles[0].thickness) {
         if (this.y > paddles[0].y &&
             this.y < (paddles[0].y + paddles[0].height)) {
 
@@ -37,11 +37,8 @@ Ball.prototype.checkCollisionsWith = function(paddles, canvas) {
             this.yVelocity = deltaY * 0.35;
             lastPaddleHit = 1;
         }
-        if (this.x < canvas.clientLeft) {
-            isBallInGoal = true;
-        }
     }
-    else if (this.x > paddles[1].x - paddles[1].thickness) {
+    else if (this.x == paddles[1].x - paddles[1].thickness) {
         if (this.y > paddles[1].y &&
             this.y < (paddles[1].y + paddles[1].height)) {
             
@@ -50,13 +47,16 @@ Ball.prototype.checkCollisionsWith = function(paddles, canvas) {
             this.yVelocity = deltaY * 0.35;
             lastPaddleHit = 2;
         }
-        if (this.x > canvas.clientWidth) {
-            isBallInGoal = true;
-        }
     }
 
     if (this.y > canvas.height || this.y < 0) {
         this.yVelocity = -this.yVelocity;
+    }
+    
+    if (this.x < canvas.clientLeft ||
+        this.x > canvas.clientWidth) {
+        
+        isBallInGoal = true;
     }
     
     return {
